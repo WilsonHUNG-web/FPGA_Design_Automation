ba3c87961895b3f951197eae505e92b7c02147ca
WilsonHUNG-web <mdx.wilsonhung@gmail.com>
2022-01-07 18:20:36 +0800

    feat: Create index.html

--- src/irl.h
+++ src/irl.h
@@ -14,6 +14,14 @@ public:
     INDEX_IRL(int horizontal_i, int vertical_j) : i(horizontal_i), j(vertical_j) {}
 };
 
+class REALI
+{
+public:
+    int x, y;
+    int w, h;
+    REALI(int x_, int y_, int w_, int h_) : x(x_), y(y_), w(w_), h(h_) {}
+};
+
 INDEX_IRL get_irl_idx(int x, int y, int s, int d);
 
 #endif
